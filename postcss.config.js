ec9b417bd583fd368a0dcc62771bf8a4ada6feba
JC Soriano <jc@juancarlosoriano.ca>
2020-06-26 17:33:56 -0400

    Modify package file to remove PostCSS config and move it into a separate config file.

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": {
+    "autoprefixer": {
+      "overrideBrowserslist": [
+        ">1%",
+        "last 4 versions",
+        "Firefox ESR",
+        "ie >= 9"
+      ]
+    }
+  }
+}
