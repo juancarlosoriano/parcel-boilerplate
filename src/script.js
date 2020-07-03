5fffc503a57ef9f9e5b6a01e308822ec0469118e
JC Soriano <jc@juancarlosoriano.ca>
2020-07-02 21:43:14 -0400

    Add cssnano and serviceWorker.js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
 import "/sass/style.scss";
+
 console.log("hello world");
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/serviceWorker.js")
+    .then(() => console.log("Service Worker Registered."));
+}
